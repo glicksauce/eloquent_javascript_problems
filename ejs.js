280f119bc614a213a963a6ea900f953ce235c77b
Jay Glickman <jayglickman@gmail.com>
2020-01-01 14:24:00 -0500

    chap. 4 recursive problem

--- ejs.js
+++ ejs.js
@@ -42,12 +42,26 @@ Write a function arrayToList that builds up a list structure like the one shown 
 If you haven’t already, also write a recursive version of nth.
 */
 
+//Chapter 4
+//Deep Comparison
+/*
+The == operator compares objects by identity. But sometimes you’d prefer to compare the values of their actual properties.
+
+Write a function deepEqual that takes two values and returns true only if they are the same value or are objects with the same properties, where the values of the properties are equal when compared with a recursive call to deepEqual.
+
+To find out whether values should be compared directly (use the === operator for that) or have their properties compared, you can use the typeof operator. If it produces "object" for both values, you should do a deep comparison. But you have to take one silly exception into account: because of a historical accident, typeof null also produces "object".
+
+The Object.keys function will be useful when you need to go over the properties of objects to compare them.
+*/
+
+/*
 const deepEqual = (val1, val2) => {
 	if (val1 === val2){
 		return true;
 	} else if (typeof val1 == 'object' && typeof val2 =='object' && val1 !=null && val2 != null){
 		if (Object.keys(val1).length == Object.keys(val2).length){
 			for (let element of Object.keys(val1)) {
+        if val1.
 				console.log("test", element, val1.element);
 			}
 		}
@@ -55,7 +69,30 @@ const deepEqual = (val1, val2) => {
 		return false;
 	}
 }
+*/
+
+const deepEqual = (val1, val2) => {
+	if (val1 === val2){
+    return true;
+    
+  //check if deep comparison is needed
+	} else if (typeof val1 == 'object' && typeof val2 =='object' && val1 !=null && val2 != null){
+    let deepTest = false;
+      for (let keys in val1){
+        //console.log(val1[keys], val2[keys]);
+        deepTest = deepEqual(val1[keys], val2[keys]);
+        if (deepTest == false){
+          return false;
+        }
+      }
+      return deepTest;
+	} 
+  return false;
+
+}
 
 let obj = {here: {is: "an"}, object: 2};
 console.log(deepEqual(obj, obj));
 console.log(deepEqual(obj, {here: 1, object: 2}));
+console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
+console.log(deepEqual({here: {is: null}, object: 2}, {here: {is: null}, object: 2}));
